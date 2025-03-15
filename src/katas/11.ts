265e9917d5fb63443214ee4bf2702386521ce0c5
Ilja Afanasev <chylmanprosto@gmail.com>
2025-03-15 12:40:34 +0300

    feat: add cata calculator

---
+++ src/katas/11.ts
@@ -0,0 +1,13 @@
+export function calculator(a,b,sign){
+    if (typeof(a) !== 'number' || typeof(b) !== 'number') {
+        return  "unknown value"
+    }
+
+    switch (sign) {
+        case '+': return a + b
+        case '-': return a - b
+        case '*': return a * b
+        case '/': return a / b
+        default: return "unknown value"
+    }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {digitize} from "./katas/10.ts";
+import {calculator} from "./katas/11.ts";
 
 
-console.log(digitize(2354364364))
+console.log(calculator(3, 2, '+'))
 
 
 createRoot(document.getElementById('root')!).render(
