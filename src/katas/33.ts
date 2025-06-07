1f34850ce672a8018b62b4a7b8aaa855e9891cc3
Ilja Afanasev <chylmanprosto@gmail.com>
2025-06-08 00:13:51 +0300

    feat: add Kata findUniq

---
+++ src/katas/33.ts
@@ -0,0 +1,15 @@
+export  function findUniq(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i + 1]) {
+            if (arr[i + 1] !== arr[i+2]) {
+                return arr[i + 1]
+                break
+            } else {
+                return arr[i]
+                break
+            }
+        }
+    }
+
+
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {humanReadable} from "./katas/31.ts";
+import {findUniq} from "./katas/33.ts";
 
-console.log(humanReadable(45296))
+console.log(findUniq([ 1, 0, 0 ]))
+console.log(findUniq([ 1, 1, 1, 2, 1, 1 ]))
+console.log(findUniq([ 0, 1, 0 ]))
 
 createRoot(document.getElementById('root')!).render(
     <App />
